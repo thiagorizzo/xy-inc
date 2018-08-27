50109f39e983f016ca7517909adfc5260c7762f5
Thiago Luiz Parrillo Rizzo <thiagorizzo@gmail.com>
2018-08-26 23:21:52 -0300

    Refresh list after registering a new poi

--- src/web/src/app/point-of-interest/point-of-interest-create.component.ts
+++ src/web/src/app/point-of-interest/point-of-interest-create.component.ts
@@ -3,6 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { PointOfInterest } from './point-of-interest';
 import { PointOfInterestService } from './point-of-interest-service';
 import { ToastrService } from 'ngx-toastr';
+import { Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-point-of-interest-create',
@@ -14,15 +16,22 @@ export class PointOfInterestCreateComponent implements OnInit {
   private pointOfInterest = <PointOfInterest> { };
   private error = undefined;
 
+  @Output()
+  pointOfInterestCreatedEvent = new EventEmitter<Event>();
+
   constructor(private pointOfInterestService : PointOfInterestService, private toastr: ToastrService) { }
 
   ngOnInit() {
   }
 
-  registerPointOfInterest() : void {
+  registerPointOfInterest(event : Event) : void {
     this.pointOfInterestService.registerPointsOfInterest(this.pointOfInterest)
                                .subscribe(
-                                  pointOfInterest => { this.pointOfInterest = pointOfInterest; this.toastr.success("Point of interest registered.") },
+                                  pointOfInterest => { 
+                                    this.pointOfInterest = pointOfInterest;
+                                    this.toastr.success("Point of interest registered.");
+                                    this.pointOfInterestCreatedEvent.emit(event)
+                                  },
                                   error => { this.toastr.error(error.message); }
                                )
   };
--- src/web/src/app/point-of-interest/point-of-interest-list.component.ts
+++ src/web/src/app/point-of-interest/point-of-interest-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { PointOfInterestService } from './point-of-interest-service';
 import { PointOfInterest } from './point-of-interest';
+import { Input } from '@angular/core';
+import { SimpleChanges } from '@angular/core';
+import { SimpleChange } from '@angular/core';
 
 @Component({
   selector: 'app-point-of-interest-list',
@@ -12,12 +15,22 @@ export class PointOfInterestListComponent implements OnInit {
   private pointsOfInterest : PointOfInterest[];
 
   constructor(private pointOfInterestService : PointOfInterestService) { }
+  
+  @Input()
+  event: Event;
 
-  ngOnInit() {
-      this.pointOfInterestService.getPointsOfInterest()
-                                 .subscribe(
-                                    pointsOfInterest => this.pointsOfInterest = pointsOfInterest
-                                 );
+  ngOnChanges(changes: SimpleChanges) {
+    this.refreshPointsOfInterest();
   }
 
+  ngOnInit() {
+    this.refreshPointsOfInterest();
+  }
+
+  refreshPointsOfInterest() : void {
+    this.pointOfInterestService.getPointsOfInterest()
+    .subscribe(
+       pointsOfInterest => this.pointsOfInterest = pointsOfInterest
+    );
+  }
 }
